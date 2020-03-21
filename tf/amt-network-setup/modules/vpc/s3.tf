ddbe7ff16ac71533864020f23cd268cde34fc3ac
Michael Meadows <michael.meadows@insight.com>
2020-03-21 14:48:35 -0400

    Added public and private route table routes to S3 endpoint

--- tf/amt-network-setup/modules/vpc/s3.tf
+++ tf/amt-network-setup/modules/vpc/s3.tf
@@ -1,6 +1,34 @@
 locals {
   s3_endpoint = var.vpc_defaults.provision_s3_vpc_endpoint ? { vpc_s3_endpoint = "true" } : {}
+  public_s3_route_tables = flatten([
+    for public_egress in local.any_public_egress : [
+      for endpoint, endpoint_value in local.s3_endpoint : {
+        public_egress = public_egress
+        endpoint      = endpoint
+      }
+    ]
+  ])
+  private_s3_route_tables = flatten([
+    for private_egress in local.private_egress_subnets : [
+      for endpoint, endpoint_value in local.s3_endpoint : {
+        private_egress = private_egress
+        endpoint       = endpoint
+      }
+    ]
+  ])
 }
+
+# output "s3_endpoint" {
+#   value = local.s3_endpoint
+# }
+
+# output "public_route_flat" {
+#   value = local.public_s3_route_tables
+# }
+
+# output "any_public_egress" {
+#   value = local.any_public_egress
+# }
 
 resource "aws_vpc_endpoint" "s3" {
   for_each = local.s3_endpoint
@@ -18,4 +46,22 @@ resource "aws_vpc_endpoint_route_table_association" "default" {
 
   route_table_id  = aws_default_route_table.default.id
   vpc_endpoint_id = aws_vpc_endpoint.s3[each.key].id
+}
+
+resource "aws_vpc_endpoint_route_table_association" "public" {
+  for_each = {
+    for value in local.public_s3_route_tables : value.public_egress => value.endpoint
+  }
+
+  route_table_id  = aws_route_table.egress_public[each.key].id
+  vpc_endpoint_id = aws_vpc_endpoint.s3[each.value].id
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private" {
+  for_each = {
+    for value in local.private_s3_route_tables : value.private_egress => value.endpoint
+  }
+
+  route_table_id  = aws_route_table.egress_private[each.key].id
+  vpc_endpoint_id = aws_vpc_endpoint.s3[each.value].id
 }
