277fcebe9d3b73816fbd96b4e373b3f21d2cede5
Michael Meadows <michael.meadows@insight.com>
2020-03-24 12:17:32 -0400

    Made S3 buckets simple again (tm)

--- tf/amt-network-setup/s3-buckets.tf
+++ tf/amt-network-setup/s3-buckets.tf
@@ -2,21 +2,5 @@ resource "aws_s3_bucket" "snapshot_bucket" {
   provider      = aws.shared
   bucket_prefix = "amt-shared-vmware-snapshot-"
   acl           = "private"
-
-  tags = module.shared_tags.tags
-}
-
-resource "aws_s3_bucket_policy" "snapshot_bucket" {
-  provider = aws.shared
-  bucket   = aws_s3_bucket.snapshot_bucket.bucket
-  policy   = templatefile("${path.module}/policies/vmware-snapshot-policy.json", { bucket_arn = aws_s3_bucket.snapshot_bucket.arn, vpc_id = module.transit_vpc.vpc_id })
-}
-
-resource "aws_s3_bucket_public_access_block" "snapshot_bucket" {
-  provider                = aws.shared
-  bucket                  = aws_s3_bucket.snapshot_bucket.bucket
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  tags          = module.shared_tags.tags
 }
