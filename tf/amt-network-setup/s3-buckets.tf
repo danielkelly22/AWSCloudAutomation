32545dcd80fb34023aa3563056bd18ebe22606bc
Michael Meadows <michael.meadows@insight.com>
2020-03-24 12:31:53 -0400

    Made bucket simple again (tm) but with policies that allow it to work

--- tf/amt-network-setup/s3-buckets.tf
+++ tf/amt-network-setup/s3-buckets.tf
@@ -4,3 +4,18 @@ resource "aws_s3_bucket" "snapshot_bucket" {
   acl           = "private"
   tags          = module.shared_tags.tags
 }
+
+resource "aws_s3_bucket_public_access_block" "snapshot_bucket" {
+  provider                = aws.shared
+  bucket                  = aws_s3_bucket.snapshot_bucket.bucket
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "snapshot_bucket" {
+  provider = aws.shared
+  bucket   = aws_s3_bucket.snapshot_bucket.bucket
+  policy   = templatefile("${path.module}/policies/snapshot-bucket.json", { bucket-arn = aws_s3_bucket.snapshot_bucket.arn })
+}
