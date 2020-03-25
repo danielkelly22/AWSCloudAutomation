9f656439b59d56a55b8cee3cd873865480131b1b
rayullery <58446474+rayullery@users.noreply.github.com>
2020-03-25 10:41:40 -0400

    Rullery/vmware dr (#10)
    * added bucket in
    
    * added in access point
    
    * test
    
    * fixing policy

--- tf/amt-network-setup/s3-buckets.tf
+++ tf/amt-network-setup/s3-buckets.tf
@@ -34,30 +34,30 @@ resource "aws_s3_bucket_public_access_block" "snapshot_bucket" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-resource "aws_s3_access_point" "snapshot_bucket" {
-  provider = aws.shared
-  bucket   = aws_s3_bucket.snapshot_bucket.bucket
-  name     = "amt-transit-vpc-access-point"
-  vpc_configuration {
-    vpc_id = "vpc-04de814c0d3cfdd96"
-  }
-  # policy = templatefile("${path.module}/policies/access-point-policy.json", { bucket_arn = aws_s3_bucket.snapshot_bucket.arn })
-  public_access_block_configuration {
-    block_public_acls       = true
-    block_public_policy     = false
-    ignore_public_acls      = true
-    restrict_public_buckets = false
-  }
-}
+# resource "aws_s3_access_point" "snapshot_bucket" {
+#   provider = aws.shared
+#   bucket   = aws_s3_bucket.snapshot_bucket.bucket
+#   name     = "amt-transit-vpc-access-point"
+#   vpc_configuration {
+#     vpc_id = "vpc-04de814c0d3cfdd96"
+#   }
+#   # policy = templatefile("${path.module}/policies/access-point-policy.json", { bucket_arn = aws_s3_bucket.snapshot_bucket.arn })
+#   public_access_block_configuration {
+#     block_public_acls       = true
+#     block_public_policy     = false
+#     ignore_public_acls      = true
+#     restrict_public_buckets = false
+#   }
+# }
 output "kms_id" {
   value = aws_kms_key.snapshot_bucket.id
 }
 output "bucket" {
   value = aws_s3_bucket.snapshot_bucket.bucket
 }
-output "access_point_arn" {
-  value = aws_s3_access_point.snapshot_bucket.arn
-}
+
 # output "access_point_arn" {
 #   value = aws_s3_access_point.snapshot_bucket.arn
 # }
+
+
